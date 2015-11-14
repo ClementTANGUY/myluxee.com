e2b7f3686d9959b55bd0f8f245aed49bf18b334a
brunosiqueira <bsiqueira@gmail.com>
2015-11-14 19:50:09 +0000

    saving filter in the session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
   before_action :require_no_sales_associate, only: [:index, :locate]
   def locate
-    @place = params[:place]
+    session[:place] = @place = params[:place] || session[:place]
     @stores = Store.includes(:brand).where("LOWER(city) = LOWER(?) or LOWER(country) = LOWER(?)", @place, @place).order("brands.name")
-    if @brand = params[:brand] and !@brand.blank?
+    if session[:brand] = @brand = params[:brand] || session[:brand] and !@brand.blank?
       @stores = @stores.where("brands.id = ?", @brand)
     end
-    if @speciality = params[:speciality] and !@speciality.blank?
+    if session[:speciality] = @speciality = params[:speciality] || session[:speciality] and !@speciality.blank?
       @stores = @stores.where("brands.speciality = ?", @speciality)
     end
-    if @address = params[:address] and !@address.blank?
+    if session[:address] = @address = params[:address] || session[:address] and !@address.blank?
       @stores = @stores.where(["LOWER(address) like LOWER(?)", "%#{@address}%" ])
     end
     @stores = @stores.page(params[:page]).per(15).all
